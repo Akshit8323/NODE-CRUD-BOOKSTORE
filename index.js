fac32b97f1c7b10fdb79b7233a9043dcaa6b61a4
Akshit8323 <159544004+Akshit8323@users.noreply.github.com>
2024-12-09 18:10:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const port = 7777;
+
+const app = express();
+const db = require("./config/db");
+const adminSchema = require("./model/firstSchema");
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded());
+
+
+app.get("/", async (req,res) => {
+    let data = await adminSchema.find({});
+    data && res.render("index", {data});
+});
+
+app.post("/addData", async (req,res) => {
+    let data = await adminSchema.create(req.body);
+    data && res.redirect("/");
+});
+
+app.get("/deleteData", async (req , res) => {
+    let data = await adminSchema.findByIdAndDelete(req.query.id);
+    data && res.redirect("/");
+});
+
+app.get("/editData", async (req , res) => {
+    let singleData = await adminSchema.findById(req.query.id);
+    singleData && res.render("edit", { singleData});
+});
+
+app.post("/updateData", async (req, res) => {
+    let update = await adminSchema.findByIdAndUpdate(req.body.id, req.body);
+    update && res.redirect("/");
+});
+
+app.listen(port , (err) => {
+    err ? console.log(err) : console.log(`server started on port`);
+})
